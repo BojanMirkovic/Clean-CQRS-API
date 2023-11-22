56629e25fa06ca58e886fa48422c563097647fd0
Bojan Mirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2023-11-22 23:55:25 +0100

    feature DeleteDog is finished, next to do is to implement DeleteDog operation in DogsControler

---
+++ Aplication/Commands/Dogs/DeleteDog/DeleteDogByIdCommand.cs
@@ -0,0 +1,22 @@
+﻿using Application.Dtos;
+using Domain.Models.Animal;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.Dogs.DeleteDog
+{
+    public class DeleteDogByIdCommand : IRequest<Dog>
+    {
+        public DeleteDogByIdCommand(DogDto dogToDelete, Guid id)
+        {
+           DogToDelete = dogToDelete;
+           Id = id;
+        }
+            public DogDto DogToDelete { get; }
+            public Guid Id { get; }      
+    }
+}
---
+++ Aplication/Commands/Dogs/DeleteDog/DeleteDogByIdCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Commands.Dogs.UpdateDog;
+using Domain.Models.Animal;
+using Infrastructure.Database;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.Dogs.DeleteDog
+{
+    internal class DeleteDogByIdCommandHandler : IRequestHandler<DeleteDogByIdCommand, Dog>
+    {
+        MockDatabase _mockDatabase;
+        public DeleteDogByIdCommandHandler(MockDatabase mockDatabase)
+        { _mockDatabase = mockDatabase; }
+        public Task<Dog> Handle(DeleteDogByIdCommand request, CancellationToken cancellationToken)
+        {
+            Dog dogToDelete = _mockDatabase.Dogs.FirstOrDefault(dog => dog.Id == request.Id)!;
+
+            dogToDelete.Name = request.DogToDelete.Name;
+
+            return Task.FromResult(dogToDelete);
+        }
+    }
+}
