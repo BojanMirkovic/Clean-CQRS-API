d624b422cb3746f56adfa06cb0fd377553a4086e
Bojan Mirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2023-11-28 20:39:38 +0100

    Domain/Model/ user is created, Aplication/Dto userDto is created, API/Controllers UserController with end point Register and Login are creatde. Next to do is creating JW token

---
+++ Aplication/Dtos/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    //we are using Dto to send data to API,password is simple string because we use it only to register/login
+    public class UserDto
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
